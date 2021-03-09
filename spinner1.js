1cc5868e781ebff39458f0419e0ef318fa4daae7
Scarlet Shao <scarletshao@gmail.com>
2021-03-09 21:17:26 +0000

    spinner2 refactor the code for spinner1

--- spinner1.js
+++ spinner1.js
@@ -17,11 +17,14 @@ setTimeout(() => {
   process.stdout.write('\r|   '); 
 }, 900);
 setTimeout(() => {
-  process.stdout.write('\r-   '); 
+  process.stdout.write('\r/   '); 
 }, 1100);
 setTimeout(() => {
-  process.stdout.write('\r\\  '); 
+  process.stdout.write('\r-  '); 
 }, 1300);
 setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, 1500);
+setTimeout(() => {
   process.stdout.write('\r|   '); 
-}, 15000);
+}, 1700);
